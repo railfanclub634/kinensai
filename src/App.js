be148d459e0f614de5c77c19aeb3c26c66b8a58b
okamoto arisa <oka1202ari@gmail.com>
2019-04-15 18:14:10 +0900

    メニューボタンを実装

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import './App.css'
 import Header from './components/header'
-import Menu from './components/header'
 
 class App extends Component {
   render() {
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,20 +2,32 @@ import React from 'react'
 import './header.css'
 class Header extends React.Component {
 
-    menuClicked() {
+    constructor(props) {
+        super(props)
+        this.state = {
+            menu: false
+        }
+    }
 
+    menuClicked = () => {
+        this.setState({ menu: !this.state.menu })
     }
     // このコンポーネントの中にメニュー作るんだぞわかったな？？？？？？
     render() {
         return (
-            <div className='header'>
-                <div className='header-text maintext'><p>97th Annivfest</p></div>
-                <div className='menu-button' onClick={this.menuClicked()}>
-                    <div className='top'></div>
-                    <div className='center'></div>
-                    <div className='bottom'></div>
+            <>
+                <div className='header'>
+                    <div className='header-text maintext'><p>97th Annivfest</p></div>
+                    <div className={this.state.menu ? 'menu-button open' : 'menu-button'} onClick={this.menuClicked}>
+                        <div className='top'></div>
+                        <div className='center'></div>
+                        <div className='bottom'></div>
+                    </div>
                 </div>
-            </div>
+                <div className={this.state.menu ? 'menu visible' : 'menu'} >
+
+                </div>
+            </>
         )
     }
 }
