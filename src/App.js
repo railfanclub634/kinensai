bbe778f0f5d0aa93d85410e04692e95e299243cc
okamoto arisa <oka1202ari@gmail.com>
2019-04-24 15:24:41 +0900

    a

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react'
-import './comingsoon.css'
+import { Route, withRouter } from 'react-router-dom'
+import './App.css'
+import Header from './components/header'
+import Home from './components/home'
 
 class App extends Component {
 
   render() {
     return (
       <>
-        <p id='a'>97th Annivfest</p>
-        <p id='b'>COMING<br />SOON</p>
-        <p id='c'>第97回武蔵高等学校中学校鉄道研究部の記念祭ホームページです。<br />公開までしばらくお待ちください。</p>
+        <Header />
+        <Route exact path='/' component={Home} />
+        {/* <Route exact path='/kenkyu' component={Kenkyu} /> */}
+        <div className='footer'>
+          <p>©2019 武蔵高等学校中学校鉄道研究部</p>
+        </div>
       </>
     )
   }
 }
 
-export default App
+export default withRouter(App)
